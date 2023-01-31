3e418dcb12cf379bc1666d2f6dadd28a35a139a8
nndwn <nandawan.libya@gmail.com>
2023-01-31 22:50:56 +0700

    update animation

--- src/components/header/slidetwo.tsx
+++ src/components/header/slidetwo.tsx
@@ -27,13 +27,15 @@ const bearSlideItemData: TBearSlideItemDataList  = Slidedata().map(node => {
         key: node.id,
         children: <BearSlideItem imageUrl={node.slide.childImageSharp.original.src}>
         <div className="bg position-absolute" style={{backgroundImage: `url(${Backgroundimage()})`}}></div>
-            <div className="headline d-grid justify-content-center align-content-center vh-100 position-relative container">
-                <div className="d-flex flex-column align-items-center container-lg">
-                { null == node.textone && aktiv ? "" : <div className="h6 fw-bold">{node.textone}</div>}
-                { null == node.texttwo && aktiv ? "" : <div className="h2 fw-bold">{node.texttwo}</div>}
-                { null == node.textthree && aktiv ? "" : <div className="h2 fw-bold">{node.textthree}</div>}
-                { null == node.textfour && aktiv ? "" : <div className="fs-6 fw-light text-break">{node.textfour}</div>}
-                { node.button[0].value + node.button[1].value > 0 && aktiv ? <div className="d-flex">
+            <div className="headline  position-relative container-lg">
+                <div className="d-flex flex-column align-items-center  vh-100">
+                <div className="d-flex flex-grow-1 flex-column align-items-center justify-content-center  mt-5">
+                    { null == node.textone && aktiv ? "" : <div className="h6 fw-bold text-center">{node.textone}</div>}
+                    { null == node.texttwo && aktiv ? "" : <div className="h2 fw-bold text-center">{node.texttwo}</div>}
+                    { null == node.textthree && aktiv ? "" : <div className="h2 fw-bold text-center">{node.textthree}</div>}
+                    { null == node.textfour && aktiv ? "" : <div className="fs-6 fw-light text-break text-center">{node.textfour}</div>}
+                </div>
+                { node.button[0].value + node.button[1].value > 0 && aktiv ? <div className="d-flex buttons mb-5">
                     {node.button[0].value > 0 && aktiv ? <Link className="left " to={node.button[0].url}>{node.button[0].name}</Link>:""}
                     {node.button[1].value > 0 && aktiv ? <Link className="right" to={node.button[1].url}>{node.button[1].name}</Link>:""}
                 </div>:null}
