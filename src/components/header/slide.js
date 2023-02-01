335bb9ed9ac8a82ac2a4f63a0b22c5d4bcded8ea
nndwn <nandawan.libya@gmail.com>
2023-02-01 18:10:31 +0700

    reponsive header

--- src/components/header/slide.js
+++ src/components/header/slide.js
@@ -27,22 +27,7 @@ export const Slide = () => {
                             className="vh-100 img-slide"
                         />
                         <div className="bg position-absolute" style={{ backgroundImage: `url(${bg})` }}></div>
-                    </Carousel.Item>
-                )
-                )
-            }
-        </Carousel>
-        <Carousel
-        pause={nopause}
-        controls={nocontrol}
-        indicators = {indicator}
-        className = "position-absolute text-fade"
-        fade
-        >
-        {
-                Slidedata().map(node => (
-                    <Carousel.Item key={node.id} interval={interval} className="text-item" >
-                        <div className="headline  position-relative container-lg">
+                        <div className="position-absolute headline container-lg">
                             <div className="d-flex flex-column align-items-center  vh-100">
                                 <div className="d-flex flex-grow-1 flex-column align-items-center justify-content-center  mt-5">
                                     { null == node.textone && aktiv ? "" : <div className="h6 fw-bold text-center">{node.textone}</div>}
@@ -50,9 +35,9 @@ export const Slide = () => {
                                     { null == node.textthree && aktiv ? "" : <div className="h2 fw-bold text-center">{node.textthree}</div>}
                                     { null == node.textfour && aktiv ? "" : <div className="fs-6 fw-light text-break text-center">{node.textfour}</div>}
                                 </div>
-                                { node.button[0].value + node.button[1].value > 0 && aktiv ? <div className="d-flex buttons mb-5">
-                                    {node.button[0].value > 0 && aktiv ? <Link className="left " to={node.button[0].url}>{node.button[0].name}</Link>:""}
-                                    {node.button[1].value > 0 && aktiv ? <Link className="right" to={node.button[1].url}>{node.button[1].name}</Link>:""}
+                                { node.button[0].value + node.button[1].value > 0 && aktiv ? <div className="row buttons justify-content-center">
+                                    {node.button[0].value > 0 && aktiv ? <Link className="left col-6 mx-2 mb-2" to={node.button[0].url}>{node.button[0].name}</Link>:""}
+                                    {node.button[1].value > 0 && aktiv ? <Link className="right col-6 mx-2 mb-2" to={node.button[1].url}>{node.button[1].name}</Link>:""}
                                 </div>:null}
                             </div>
                         </div>
